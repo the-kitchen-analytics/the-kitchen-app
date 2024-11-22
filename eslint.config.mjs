c8b8485c1374083c399f2178a5b746fabaeb7753
Hleb Ramniou <g.remniov@gmail.com>
2024-11-23 00:31:48 +0200

    Fix formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,57 +1,57 @@
-import react from "eslint-plugin-react";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import react from 'eslint-plugin-react'
+import globals from 'globals'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
 
-export default [...compat.extends("eslint:recommended", "plugin:react/recommended"), {
-    plugins: {
-        react,
+export default [...compat.extends('eslint:recommended', 'plugin:react/recommended'), {
+  plugins: {
+    react,
+  },
+
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.node,
     },
 
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
 
-        ecmaVersion: "latest",
-        sourceType: "module",
+  settings: {
+    react: {
+      version: 'detect',
     },
+  },
 
-    settings: {
-        react: {
-            version: "detect",
-        },
-    },
+  rules: {
+    indent: ['error', 2, {
+      SwitchCase: 1,
+    }],
 
-    rules: {
-        indent: ["error", 2, {
-            SwitchCase: 1,
-        }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'object-curly-spacing': ['warn', 'always'],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
 
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "single"],
-        semi: ["error", "never"],
-        "object-curly-spacing": ["warn", "always"],
-        "react/jsx-uses-react": "off",
-        "react/react-in-jsx-scope": "off",
-        "react/prop-types": "off",
-
-        "react/jsx-tag-spacing": ["warn", {
-            closingSlash: "never",
-            beforeSelfClosing: "always",
-            afterOpening: "never",
-            beforeClosing: "allow",
-        }],
-    },
-}];
+    'react/jsx-tag-spacing': ['warn', {
+      closingSlash: 'never',
+      beforeSelfClosing: 'always',
+      afterOpening: 'never',
+      beforeClosing: 'allow',
+    }],
+  },
+}]
